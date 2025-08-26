9b9346c18ec5d5408864cd0d4678ca8abd398052
DevErika <erikamontoya2023@gmail.com>
2025-08-27 01:17:29 +0200

    resolve conflict with App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import Header from "./components/Header/Header";
 
 import Intro from "./pages/Intro.jsx";
-
-
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         {/* Página de inicio */}
         <Route path="/" element={<Intro />} />
