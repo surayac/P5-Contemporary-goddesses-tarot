aa7446bcb548c2c9662993a1edaedffa22626546
surayac <surayacsm@gmail.com>
2025-08-29 10:13:21 +0200

    editing cards and deck code

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -17,37 +17,42 @@ const Deck = () => {
   }, []);
 
   return (
-    <section className="min-h-screen flex flex-col items-center justify-center px-4">
-      <div className="text-center mb-8">
-        <h1 className="text-white text-2xl md:text-3xl font-bold">
-          Bienvenido, {playerName}!
-        </h1>
+    <main className="min-h-screen flex flex-col justify-center px-4">
+      <section className="flex justify-between items-start w-full mb-8">
+        <p className="text-white text-left text-1xl md:text-1xl"> Bienvenido, {playerName}!</p>
         {lastDate && (
-          <p className="text-[#D3A85D] mt-2">
-            Última tirada: <strong>{lastDate}</strong>
-          </p>
+          <p className="text-[#D3A85D] text-right text-1xl md:text-1xl">
+            Última tirada: {lastDate}</p>
         )}
-      </div>
+      </section>
 
-      <div
-        className="inline-flex"
-        style={{ ["--overlap"]: "clamp(12px, 2.5vw, 48px)" }}
-      >
-        {cards.map((card, index) => (
-          <div
-            key={card.id ?? index}
-            style={{ marginLeft: index === 0 ? 0 : "calc(var(--overlap) * -1)" }}
-            className="
+      <section className="text-center mb-6">
+        <h1 className="font-metamorphous text-4xl md:text-4xl text-[#FFDBB7] mb-10 mt-15">
+          Escoge tres cartas </h1>
+
+        <div
+          className="inline-flex"
+          style={{ ["--overlap"]: "clamp(12px, 2.5vw, 48px)" }}
+        >
+          {cards.map((card, index) => (
+            <div
+              key={card.id ?? index}
+              style={{ marginLeft: index === 0 ? 0 : "calc(var(--overlap) * -1)" }}
+              className="
               transition-transform duration-200
               hover:-translate-y-2 hover:z-20 hover:scale-[1.02]
               focus-within:-translate-y-2 focus-within:z-20
             "
-          >
-            <Card card={card} />
-          </div>
-        ))}
-      </div>
-    </section>
+            >
+              <Card card={card} />
+            </div>
+          ))}
+        </div>
+        <button
+          onClick={() => navigate("/cards", { state })}
+          className="h-12 px-6 rounded-xl text-black hover:text-white bg-[#FFDBB7] hover:bg-[#5D688A] border border-black cursor-pointer text-xl w-full mt-10 sm:w-auto">"botón para llevar a la pagina de cartas"</button>
+      </section>
+    </main>
   );
 };
 
