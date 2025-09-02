6366428d6c8f2168f1b54e6830583c9a0cbd6844
Abaraira <cibersteficodes@gmail.com>
2025-09-02 15:38:15 +0200

    added mobile behavior to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,13 +9,13 @@ function Header() {
                 <img src={logo} alt="Logo" 
                 className="ml-1 md:ml-auto w-12 md:w-24 h-auto transition-all duration-300" />
             </a>
-        <div className="flex gap-16 text-[var(--color-starDust)] text-sm md:text-lg font-metamorphous">
+        <div className="flex flex-wrap gap-8 md:gap-16 text-[var(--color-starDust)] text-sm md:text-lg font-metamorphous">
             <a href="/" 
-              className="hover:drop-shadow-[0_0_8px_#FFDBB7] hover:scale-110 transition-all duration-300" >
+              className="md:hover:drop-shadow-[0_0_8px_#FFDBB7] active:drop-shadow-[0_0_8px_#FFDBB7] active:scale-110 hover:scale-110 transition-all duration-300" >
             INICIO
             </a>
             <a href="/Favorites" 
-              className="hover:drop-shadow-[0_0_8px_#FFDBB7] hover:scale-110 transition-all duration-300"  >
+              className="md:hover:drop-shadow-[0_0_8px_#FFDBB7] active:drop-shadow-[0_0_8px_#FFDBB7] active:scale-110 hover:scale-110 transition-all duration-300" >
             HISTORIAL
             </a>
         </div>
